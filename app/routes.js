3e18f912d1448f7ca2487d5917dc138b74d53de0
mikeycattell <michael.a.cattell@gmail.com>
2019-08-21 11:10:38 +0100

    big update
    Narrowed down the prototype to one folder for ease (agent/sprint 26).
    
    Hopefully covered all agent work off to a level for testing.

--- app/routes.js
+++ app/routes.js
@@ -40,3 +40,97 @@ router.post('/check-eligibility-monthly', (req, res) => {
 });
 
 module.exports = router
+
+router.post('/sprint26/agent/confirm-answer', function (req, res) {
+
+  let agent = req.session.data['agent']
+
+  if (agent === 'true') {
+    res.redirect('notify-claimant-no')
+  } else {
+    res.redirect('check-elig-pay')
+  }
+})
+
+router.post('/sprint26/agent/confirm-answer', function (req, res) {
+
+  let agents = req.session.data['agents']
+
+  if (agents === 'true') {
+    res.redirect('notify-claimant-no')
+  } else {
+    res.redirect('check-elig-pay')
+  }
+})
+
+router.post('/sprint26/agent/claimant-carer-answer', function (req, res) {
+
+  let claimantcareranswer = req.session.data['claimantcareranswer']
+
+  if (claimantcareranswer === 'true') {
+    res.redirect('notify-claimant-no')
+  } else {
+    res.redirect('success-exclude123213')
+  }
+})
+
+router.post('/sprint26/agent/verify-claimant', function (req, res) {
+
+  let verifyclaimant = req.session.data['verifyclaimant']
+
+  if (verifyclaimant === 'true') {
+    res.redirect('check-carer-element')
+  } else {
+    res.redirect('success-exclude11')
+  }
+})
+
+module.exports = router
+
+router.post('/sprint26/agent/edit-notify-claimant', function (req, res) {
+
+  let editnotifyclaimant = req.session.data['editnotifyclaimant']
+
+  if (editnotifyclaimant === 'true') {
+    res.redirect('check-carer-history')
+  } else {
+    res.redirect('check-carer-element')
+  }
+})
+
+module.exports = router
+
+router.post('/sprint26/agent/raise-edit-payment', function (req, res) {
+
+  let wantsPayment = req.session.data['wantsPayment']
+
+  if (wantsPayment === 'true') {
+    res.redirect('check-carer-history')
+  } else {
+    res.redirect('check-elig-pay')
+  }
+})
+
+module.exports = router
+
+router.post('/sprint26/agent/claimant-cap-LCWRA', function (req, res) {
+
+  let isInLcwra = req.session.data['isInLcwra']
+
+  if (isInLcwra === 'true') {
+    res.redirect('claimant-eligible')
+  } else {
+    res.redirect('claimant-eligible-single-LCWRAno')
+  }
+})
+
+router.post('/sprint26/agent/claimant-single-couple', function (req, res) {
+
+  let relationshipChanged = req.session.data['relationshipChanged']
+
+  if (relationshipChanged === 'true') {
+    res.redirect('notify-claimant-no')
+  }
+})
+
+module.exports = router
