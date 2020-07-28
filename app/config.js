4317cff7d5a143c65eaf16abe8a0bdb3d28050fa
mikeycattell <michael.a.cattell@gmail.com>
2020-07-28 15:26:29 +0100

    Updating page title from gov uk prototype to service name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Severe Disability Premium',
+  serviceName: 'Manage Transitional Payments for Universal Credit',
 
   // Default port that prototype runs on
   port: '3000',
